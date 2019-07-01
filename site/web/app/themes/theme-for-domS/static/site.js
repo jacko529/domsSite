59adf9663650feb4c4457475a6848013618a6f06
Niamh Franklyn <niamh@3sidedcube.com>
2019-07-01 07:16:11 +0100

    add

--- site/web/app/themes/theme-for-domS/static/site.js
+++ site/web/app/themes/theme-for-domS/static/site.js
@@ -1,7 +1,16 @@
 jQuery( document ).ready( function( $ ) {
 
 
-
+  $('.hamburger').click(function(){
+    $(this).toggleClass('is-active')
+    if ($(this).hasClass('is-active')){
+      $('.overlay').addClass('is-open no-delay translate').removeClass('delay')
+      $('.overlay ul li').addClass('is-shown delay').removeClass('no-delay')
+    }else{
+      $('.overlay').removeClass('is-open no-delay translate').addClass('delay')
+      $('.overlay ul li').removeClass('is-shown delay').addClass('no-delay')
+    }
+  })
 
 
 
